21cebeac98c7991c6bb9df63e13044ac34509e15
unknown <https://github.com/MuminovMuslimbek>
2024-11-21 14:22:33 +0300

    commit done

--- src/App.jsx
+++ src/App.jsx
@@ -5,61 +5,70 @@ import Movieslist from './components/Movieslist/movies.json';
 import heart from "./assets/heart.svg";
 import heartSolid from "./assets/heart-solid.svg";
 
-const App = () => {
-  const [like, setLike] = useState(0)
-  const [solid, setSolid] = useState(false)
+const MoviesComponent = ({ Movieslist }) => {
+  const [likedMovies, setLikedMovies] = useState({});
+  const [count, setCount] = useState(0);
 
-  function hendleClick(arg) {
-    let cope = [...Movieslist]
-    cope = cope.map((v) => {
-      if (v.id==arg) {
-        setSolid(!solid)
-      }
-    })
+  const handleClick = (movieId) => {
+    setLikedMovies((prev) => {
+      const isLiked = !prev[movieId];
+      setCount((prevCount) => prevCount + (isLiked ? 1 : -1));
+      return {
+        ...prev,
+        [movieId]: isLiked,
+      };
+    });
+  };
 
-  }
   return (
     <div>
-      <Header data={like} />
+      <Header count={count} />
       <div className="containerMovies">
         <h1>Movies</h1>
         <div className="wrapperMovies">
-          {
-            Movieslist.map((movie, index) => (
-              <div className="cardMovie" key={index}>
-                <img src={movie.Images} alt="Movie Poster" />
-                <div className="titleMovies">
-                  <h3>{movie.Title}</h3>
-                  <p>{movie.Plot}</p>
-                  <button onClick={() => { hendleClick(movie.id) }} className="like">
-                    {
-                      solid ? <img src={heartSolid} alt="Heart Icon" /> :
-                        <img src={heart} alt="Heart Icon" />
-                    }
-                  </button>
-                  <ul className="commentMovies">
-                    <span className="spanId">{movie.id}</span>
-                    <li><strong>Yil:</strong> {movie.Year}</li>
-                    <li><strong>Reyting:</strong> {movie.Rated}</li>
-                    <li><strong>Chiqarilgan sana:</strong> {movie.Released}</li>
-                    <li><strong>Davomiyligi:</strong> {movie.Runtime}</li>
-                    <li><strong>Janr:</strong> {movie.Genre}</li>
-                    <li><strong>Rejissor:</strong> {movie.Director}</li>
-                    <li><strong>Yozuvchi:</strong> {movie.Writer}</li>
-                    <li><strong>Aktyorlar:</strong> {movie.Actors}</li>
-                    <li><strong>Til:</strong> {movie.Language}</li>
-                    <li><strong>Mamlakat:</strong> {movie.Country}</li>
-                    <li><strong>Mukofotlar:</strong> {movie.Awards}</li>
-                    <li><strong>IMDb reytingi:</strong> {movie.imdbRating}</li>
-                  </ul>
-                </div>
+          {Movieslist.map((movie, index) => (
+            <div className="cardMovie" key={index}>
+              <img src={movie.Images[0]} alt="Movie Poster" />
+              <div className="titleMovies">
+                <h3>{movie.Title}</h3>
+                <p>{movie.Plot}</p>
+                <button onClick={() => handleClick(movie.id)} className="like">
+                  {likedMovies[movie.id] ? (
+                    <img src={heartSolid} alt="Heart Icon" />
+                  ) : (
+                    <img src={heart} alt="Heart Icon" />
+                  )}
+                </button>
+                <ul className="commentMovies">
+                  <span className="spanId">{movie.id}</span>
+                  <li><strong>Yil:</strong> {movie.Year}</li>
+                  <li><strong>Reyting:</strong> {movie.Rated}</li>
+                  <li><strong>Chiqarilgan sana:</strong> {movie.Released}</li>
+                  <li><strong>Davomiyligi:</strong> {movie.Runtime}</li>
+                  <li><strong>Janr:</strong> {movie.Genre}</li>
+                  <li><strong>Rejissor:</strong> {movie.Director}</li>
+                  <li><strong>Yozuvchi:</strong> {movie.Writer}</li>
+                  <li><strong>Aktyorlar:</strong> {movie.Actors}</li>
+                  <li><strong>Til:</strong> {movie.Language}</li>
+                  <li><strong>Mamlakat:</strong> {movie.Country}</li>
+                  <li><strong>Mukofotlar:</strong> {movie.Awards}</li>
+                  <li><strong>IMDb reytingi:</strong> {movie.imdbRating}</li>
+                </ul>
               </div>
-            ))
-          }
+            </div>
+          ))}
         </div>
       </div>
     </div>
   );
-}
+};
+
+const App = () => {
+  return (
+    <div>
+      <MoviesComponent Movieslist={Movieslist} />
+    </div>
+  );
+};
 
 export default App;
--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,23 +1,31 @@
-import './index.css'
+import './index.css';
 import movie from "../../assets/movie.png";
 import heart from "../../assets/heart.svg";
 
-const index = () => {
+const Header = ({ count }) => {
   return (
     <div>
-      <header className='header'>
+      <header className="header">
         <div className="header__container container">
-          <a className='element' href="../index.html"><h1 className='element'><img src={movie} className='logo' />Kino Ro'yxati</h1></a>
+          <a className="element" href="../index.html">
+            <h1 className="element">
+              <img src={movie} className="logo" alt="Logo" />
+              Kino Ro'yxati
+            </h1>
+          </a>
           <ul>
             <li><a href="#">Bosh sahifa</a></li>
             <li><a href="#">Kinolar</a></li>
             <li><a href="#">Yangiliklar</a></li>
           </ul>
-          <div className='likes'><img src={heart}/><span className='count'>0</span> likes</div>
+          <div className="likes">
+            <img src={heart} alt="Heart Icon" />
+            <span className="count">{count > 0 ? count : 0}</span> likes
+          </div>
         </div>
       </header>
     </div>
-  )
-}
+  );
+};
 
-export default index
+export default Header;
